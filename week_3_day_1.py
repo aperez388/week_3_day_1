50c314f06a5261fa9fc8cdebc37d4b69393c9dc7
Perez <aperez388@cps.k12.il.us>
2024-09-10 14:47:36 -0500

    first commit

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -1,3 +1,4 @@
+
 # # Week3
 # # This week we will work on :
 # # Working With Strings
@@ -8,4 +9,33 @@
 
 # # 1. Building a Basic Calculator
 # # 2. Mad Libs Game
-x = 20
+
+
+
+
+
+# # Review
+# create variables for the following:
+# 1. age
+age = 16
+# 2. name
+name = "Anabel"
+# 3. song
+song = "Dance Monkey"
+# 4. food
+food = "gorditas"
+# 5. number
+number = 944
+
+# #now include the variables you just made print the following ...
+
+
+# Once upon a time, there was a [age] year old coder named [name].
+print("Once upon a time, there was a " + str(age) + " year old coder named " + name + ".")
+# [name] liked to hum the song [song] while coding. It was so annoying
+# that their teammates would throw [food] until [name] would stop singing.
+print(name + " liked to hum the song " + song + " while coding. It was so annoying that their teammates would throw " + food + " until " + name +  "would stop singing.")
+# Still, [name] was the best coder on the team and could write [number] lines of code every day. 
+print("Still, " + name + " was the best coder on the team and could write " + str(number) + " lines of code every day.")
+# Maybe [song] was [name]'s secret power?
+print("Maybe " + song + " was " + name + "'s secret power?")
